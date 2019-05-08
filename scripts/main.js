78606be8894da68690b35c658bb1b060e6c2a35f
Diana Dubovan <diana.dubovan@gmail.com>
2019-05-07 23:12:13 -0400

    waiting for api key

--- scripts/main.js
+++ scripts/main.js
@@ -13,82 +13,17 @@ var countdown = setInterval(function() {
     var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     //Display countdown
-    document.getElementById("countdown").innerText = days + "days" + hours + "hours"
-    + minutes + "minutes" + seconds + "seconds";
+    document.getElementById("countdown").innerText = days + "days " + hours + "hours "
+    + minutes + "minutes " + seconds + "seconds";
 
     if (timeDifference === 0){
         clearInterval(x);
         document.getElementById("countdown").innerText = "It's finally summer!!";
     }
+
 }, 1000);
 
 var temp = document.getElementById("temperature");
 temp.addEventListener('click', (event) => {
     event.target.toggle('done');
   }, false);
-    
-
-
-
-
-
-// //To display current time as a reference
-// var showCurrentTime = function(){
-//     var clock = document.getElementById("clock");
-//     var currentTime = new Date();
-//     var hours = currentTime.getHours();
-//     var minutes = currentTime.getMinutes();
-//     var seconds = currentTime.getSeconds();
-//     var meridian = "AM";
-
-//     //Set time in hours
-//     if (hours >= noon){
-//         meridian = "PM";
-//     }
-
-//     if (hours > noon){
-//         hours = hours - 12;
-//     }
-
-//     //Set time in minutes
-//     if (minutes < 10){
-//         minutes = "0" + minutes;
-//     }
-
-//     //Set time in seconds
-//     if (seconds < 10){
-//         seconds = "0" + seconds;
-//     }
-
-//     //Put together hours, minutes, and seconds
-//     var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
-
-//     clock.innerText = clockTime;
-// };
-
-// //Setting the clock to decrease
-// var updateClock = function(){
-//     var time = new Date().getHours();
-//     var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-
-//     if (time === partytime){
-//         image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-//         messageText = "It's almost here!";
-//     }
-//     else if (time === wakeuptime){
-//         image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-//         messageText = "Almost there!";
-//     }
-
-//     console.log(messageText);
-//     timeEventJS.innerText = messageText;
-
-//     showCurrentTime();
-// };
-
-// updateClock();
-
-// //Have the clock decrease 1 second at a time
-// var oneSecond = 1000;
-// setInterval(updateClock, oneSecond);
-
